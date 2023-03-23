34d61d5aeeb5db6df2d137cde9d33866f16b2f63
Jacob M Sock <jacobmsock@gmail.com>
2023-03-23 08:37:52 -0400

    Initial Commit

---
+++ CharacterWithLongestRepetition/CharacterWithLongestRepetitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CharacterWithLongestRepetitionApp.swift
+//  CharacterWithLongestRepetition
+//
+//  Created by Jacob Sock on 3/23/23.
+//
+
+import SwiftUI
+
+@main
+struct CharacterWithLongestRepetitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
